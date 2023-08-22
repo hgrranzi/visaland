f3220a5dc2b18989146d2566bf6f34fb94be59a0
hgrranzi <vkh.mariia@gmail.com>
2023-08-22 16:28:32 +0300

    main app class created

---
+++ src/main/java/com/hgrranzi/visaland/VisalandApplication.java
@@ -0,0 +1,12 @@
+package com.hgrranzi.visaland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisalandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisalandApplication.class);
+    }
+}
