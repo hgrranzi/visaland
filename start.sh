226299f2496bfaab46e23fa652df3279c952e0de
hgrranzi <uteena@student.42.fr>
2024-02-13 17:32:27 +0100

    docker naming change

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ docker rmi $(docker images -qa)
 docker volume rm $(docker volume ls -q)
 docker network rm $(docker network ls -q) 2>/dev/null
 
-docker-compose -f docker-compose.yaml -p module14 up -d
+docker-compose -f docker-compose.yaml -p visaland up -d
